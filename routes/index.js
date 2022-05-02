6fe7d1c2e88df0ae0e0fad908f0465fadcc20ce6
kn516 <kn516@georgetown.edu>
2022-05-02 14:48:51 -0400

    Changed landing page and sequence of nav bar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("URL PARAMS:", req.query)
+  var name = req.query.name || "World" // double pipes is an OR operator that allows us to use a default value if the url params are null / not specified
+  var message = "Hello, " + name
+  res.render('hello', { message: message });
 });
 
 /* GET about page. */
